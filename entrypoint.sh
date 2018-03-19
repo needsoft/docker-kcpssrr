08dcd60fe93c75c3e3ba6f39cb473706614d4469
needsoft <needsoft@gmail.com>
2018-03-19 20:20:42 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+KCP_CONFIG=${KCP_CONFIG:-""}
+KCP_MODULE=${KCP_MODULE:-"kcpserver"}
+KCP_FLAG=${KCP_FLAG:-"false"}
+
+while getopts "k:e:x" OPT; do
+    case $OPT in
+        k)
+            KCP_CONFIG=$OPTARG;;
+        e)
+            KCP_MODULE=$OPTARG;;
+        x)
+            KCP_FLAG="true";;
+    esac
+done
+
+if [ "$KCP_FLAG" == "true" ] && [ "$KCP_CONFIG" != "" ]; then
+    echo -e "\033[32mStarting kcptun......\033[0m"
+    $KCP_MODULE $KCP_CONFIG 2>&1 &
+else
+    echo -e "\033[33mKcptun not started......\033[0m"
+fi
+
+if [ "$SS_CONFIG" != "" ]; then
+    echo -e "\033[32mStarting shadowsocks......\033[0m"
+    python /shadowsocks/server.py
+else
+    echo -e "\033[31mError: SS_CONFIG is blank!\033[0m"
+    exit 1
+fi
